0b89fca297df4e11dbb0c1008ea9ef458b92d7c1
liupandeng <345433326@qq.com>
2018-12-17 15:10:27 +0800

    离线计算项目

---
+++ src/cn/edu360/app/log/mr/GlobalConstants.java
@@ -0,0 +1,7 @@
+package cn.edu360.app.log.mr;
+
+public class GlobalConstants {
+	
+	public static final String HEADER = "header";
+
+}
---
+++ test/cn/edu360/test/TimeTest.java
@@ -0,0 +1,14 @@
+package cn.edu360.test;
+
+import java.util.Date;
+
+public class TimeTest {
+	
+	public static void main(String[] args) {
+		
+		Date date2 = new Date(1502683439386L+490*24*60*60*1000L);
+		System.out.println(date2);
+		
+	}
+
+}
